4eb7952ef4868b2b7ecc8c8652ee680533ccb604
Hiroaki UKAJI <hiroaki@hiroakis-macbook-pro.sic.ecl.ntt.co.jp>
2015-12-21 17:19:24 +0900

    Delete Redundant Explanation

--- plugin.go
+++ plugin.go
@@ -3,20 +3,15 @@ package main
 import (
 	"fmt"
 
-	// CF Pluginを書くために必要なパッケージ
 	"github.com/cloudfoundry/cli/plugin"
 )
 
 type MyFirstPlugin struct{}
 
-// 実際の Plugin の処理はここに書かれていきます。
-// cliConnection は(おおまかに言うと) cf CLI の各種コマンドを呼び出すためのものです。
-// args には Plugin を実行する際のコマンドライン引数が入ります。
 func (mfp *MyFirstPlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	fmt.Println("Hello, CF Plugin!")
 }
 
-// Plugin の metadata(名前、バージョン、コマンドの説明等) を設定
 func (mfp *MyFirstPlugin) GetMetadata() plugin.PluginMetadata {
 	return plugin.PluginMetadata{
 		Name: "MyFirstPlugin",
